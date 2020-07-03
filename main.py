a3666948d93bfb4a6e5684bb55c527b0084beb82
Jefferson Gomes <jeffsilvaygo@gmail.com>
2020-07-03 17:17:31 -0300

    Removed the list with comparison and movement information

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#---------------------------------------------------
+#                   Imports
+#---------------------------------------------------
 from src.dev.ordenacao.ordenacao_insercao import Insert
 from src.dev.ordenacao.ordenacao_bubblesort import *
 from src.dev.ordenacao.ordenacao_quicksort import *
@@ -6,10 +9,12 @@ from src.dev.utils.log import Log
 from src.dev.visual.view import *
 from src.dev.utils.ordenacao import *
 
+
 class Principal:
 
     def __init__(self):
         self.start_order = Ordenacao()
+        self.start_ordering()
     
     def start_ordering(self):
         Log().escrever("Iniciando o processo de ordenação...")
@@ -20,24 +25,14 @@ class Principal:
 
         Log().escrever("Finalizado o processo de ordenação\n")
 
+        self.start_gui()
+
+
+    def start_gui(self):
+        View().createView(self.start_order.get_data())
+
 
 if __name__ == '__main__':
+    Principal()
+
     
-    initi = Principal()
-    initi.start_ordering()
-
-    data = [
-        
-        ['Inserção', 'Comparações', initi.start_order.get_comparacoes_insercao()[0], initi.start_order.get_comparacoes_insercao()[1]],
-        ['Inserção', 'Movimentações', initi.start_order.get_movimentacoes_insercao()[0], initi.start_order.get_movimentacoes_insercao()[1]],
-        ['BubbleSort', 'Comparações', initi.start_order.get_comparacoes_bubblesort()[0], initi.start_order.get_comparacoes_bubblesort()[1]],
-        ['BubbleSort', 'Movimentações', initi.start_order.get_movimentacoes_bubblesort()[0], initi.start_order.get_movimentacoes_bubblesort()[1]],
-        ['QuickSort', 'Comparações', initi.start_order.get_comparacoes()[0], initi.start_order.get_comparacoes()[1]],
-        ['QuickSort', 'Movimentações', initi.start_order.get_movimentacoes()[0], initi.start_order.get_movimentacoes()[1]]
-    ]
-
-    View().createView(data)
-
-       
-    
-
